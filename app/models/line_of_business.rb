46e65a8f0344f1a27f603b308a1d2e659dd92b22
Von Christian Halip <vc.halip@gmail.com>
2017-01-18 14:29:56 +0800

    Add business tax to line of business.

--- app/models/line_of_business.rb
+++ app/models/line_of_business.rb
@@ -1,8 +1,9 @@
 class LineOfBusiness < ApplicationRecord
   has_one :mayors_permit_fee, as: :feeable, class_name: "Fees::MayorsPermitFee"
+  has_one :business_tax, as: :taxable, class_name: "Taxes::BusinessTax"
   belongs_to :line_of_business_category
 
   validates :name, presence: true, uniqueness: true
 
-  accepts_nested_attributes_for :mayors_permit_fee
+  accepts_nested_attributes_for :mayors_permit_fee, :business_tax
 end
--- spec/models/line_of_business_spec.rb
+++ spec/models/line_of_business_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe LineOfBusiness, type: :model do
   describe "associations" do
     it { is_expected.to belong_to :line_of_business_category }
     it { is_expected.to have_one :mayors_permit_fee }
-
+    it { is_expected.to have_one :business_tax }
   end
 
   describe "validations" do
@@ -14,6 +14,7 @@ RSpec.describe LineOfBusiness, type: :model do
 
   describe "nested_attributes" do
     it { is_expected.to accept_nested_attributes_for(:mayors_permit_fee) }
+    it { is_expected.to accept_nested_attributes_for(:business_tax) }
   end
 
 end
