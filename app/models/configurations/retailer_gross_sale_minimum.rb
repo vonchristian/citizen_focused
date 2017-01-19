36b6f7a189b88a7960defa8957eec158f981bc53
Von Christian Halip <vc.halip@gmail.com>
2017-01-19 14:31:32 +0800

    Update gross sale.

--- app/models/configurations/retailer_gross_sale_minimum.rb
+++ app/models/configurations/retailer_gross_sale_minimum.rb
@@ -1,5 +1,9 @@
 module Configurations
   class RetailerGrossSaleMinimum < ApplicationRecord
     validates :amount, presence: true, numericality: true
+
+    def self.default_amount
+      all.last.amount || 0
+    end
   end
 end
